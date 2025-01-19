90651df4f03c9878b16560f019df23e0e75ddea3
boyd-dev <boyd-dev@foo.com>
2025-01-19 13:30:59 +0900

    SHA256

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 ```
 .\gradlew.bat run
 ```
+
+- 로컬에 다운로드(`workspace`에서)  
+
+```
+git clone https://github.com/boyd-dev/empty-gradle.git
+```
+
+다운로드 후에 `Gradle > Refresh Gradle Project`  
+이클립스의 Gradle 전역설정은 `Gradle wrapper` 선택
---
+++ src/main/java/com/foo/example/MySha256.java
@@ -0,0 +1,32 @@
+package com.foo.example;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+
+public class MySha256 {
+
+	public static void main(String[] args) {
+		
+		String str = "Alice gave 0.015BTC to EDIYA";
+		
+		try {
+			
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] encodedhash = digest.digest(str.getBytes(StandardCharsets.UTF_8));
+			
+			StringBuffer sb = new StringBuffer(); 
+
+			for(int i = 0 ; i < encodedhash.length ; i++){
+				sb.append(Integer.toString((encodedhash[i]&0xff) + 0x100, 16).substring(1));
+			}
+			
+			System.out.println(sb.toString());
+			
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
